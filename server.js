0d1472433d36347f70039807cb4e6578317f05ed
crisc97 <cristiancastanares1997@gmail.com>
2026-04-18 20:51:26 -0300

    Actualizando enlace dsports y parametros

--- server.js
+++ server.js
@@ -7,25 +7,32 @@ app.use(cors());
 
 // --- BASE DE DATOS DE CANALES ---
 const dbCanales = {
-    // Aquí pegamos la parte de la URL antes del signo "?"
-    'dsports': 'https://j8nte.envivoslatam.org/dsports/tracks-v1a1/mono.m3u8',
-    'tnt': 'https://j8nte.envivoslatam.org/hotflix/tntsports/index.m3u8',
-    'espn': 'https://j8nte.envivoslatam.org/hotflix/espnpremium/index.m3u8',
-    'fox': 'https://j8nte.envivoslatam.org/hotflix/foxsports/index.m3u8'
+    'dsports': {
+        base: 'https://j8nte.envivoslatam.org/dsports/tracks-v1a1/mono.m3u8',
+        parametros: 'ip=181.228.90.195&token=0b8484e94d78af9184c60015330fa06fbff9749e-3c-1776600035-1776546035'
+    },
+    'tnt': {
+        base: 'https://bd2ih.envivoslatam.org/tntsports/tracks-v1a1/mono.m3u8',
+        parametros: 'ip=181.228.90.195&token=8e2d37e7d6ff7c74dfb48cec3b61ebdb43c1c62a-0d-1776600674-1776546674'
+    },
+    'espn': {
+        base: 'https://kh5vy.envivoslatam.org/espnpremium/tracks-v1a1/mono.m3u8',
+        parametros: 'ip=181.228.90.195&token=d62ed86a56a0d0ff40d8feeb4b0590f74c83c28c-12-1776601177-1776547177' // Actualizar cuando lo busques
+    },
+    'fox': {
+        base: 'https://wf6kt.envivoslatam.org/foxsports/tracks-v1a1/mono.m3u8',
+        parametros: 'ip=181.228.90.195&token=9aaf0c939f786e629d2fa948addcd760f6c1a75a-cb-1776601238-1776547238' // Actualizar cuando lo busques
+    }
 };
 
 app.get('/api/get-stream/:canal', (req, res) => {
     const canalId = req.params.canal;
-    const baseUrl = dbCanales[canalId];
+    const datosCanal = dbCanales[canalId];
     
-    // --- AQUÍ PEGAS LO QUE VA DESPUÉS DEL "?" ---
-    // He incluido el IP y el Token que me pasaste
-    const parametros = "ip=181.228.90.195&token=0b8484e94d78af9184c60015330fa06fbff9749e-3c-1776600035-1776546035";
-    
-    if (baseUrl) {
+    if (datosCanal) {
         res.json({ 
             exito: true, 
-            url: `${baseUrl}?${parametros}` 
+            url: `${datosCanal.base}?${datosCanal.parametros}` 
         });
     } else {
         res.status(404).json({ exito: false, mensaje: "Canal no encontrado" });
